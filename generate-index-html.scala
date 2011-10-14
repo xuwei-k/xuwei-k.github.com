fa56c8d346aaa1bcb39e84a35d6c27fd1d779465
xuwei-k <6b656e6a69@gmail.com>
2011-10-14 12:30:21 +0900

    user name

--- generate-index-html.scala
+++ generate-index-html.scala
@@ -1,25 +1,24 @@
 import scala.io.Source
 import scala.util.parsing.json.JSON.parseFull
 
-type JsonType = Map[String,List[Map[String,String]]]
-
 def getJson[T](url:String):T = {
   val str  = Source.fromURL(url,"UTF-8").mkString  
   parseFull(str).get.asInstanceOf[T]
 }
 
 def getRepositories(user:String):List[String] = {
+  type JsonType = Map[String,List[Map[String,String]]]
   val json = getJson[JsonType]("https://github.com/api/v2/json/repos/show/" + user)  
   json("repositories").map{_.apply("url").replace("https://github.com/" + user + "/","")}
 }
 
-def hasGithubPage(repository:String):Boolean = {
-  val json = getJson[Map[String,Map[String,String]]]("https://github.com/api/v2/json/repos/show/xuwei-k/"+repository+"/branches") 
+def hasGithubPage(user:String,repository:String):Boolean = {
+  val json = getJson[Map[String,Map[String,String]]]("https://github.com/api/v2/json/repos/show/"+user+"/"+repository+"/branches") 
   json("branches").contains("gh-pages")
 }
 
 def githubPageList(user:String) = {
-  getRepositories(user).filter{ hasGithubPage }
+  getRepositories(user).filter{ r => hasGithubPage(user,r) }
 }
 
 def html(user:String) = {
