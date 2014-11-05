28a72a25e856e8d89a133ebdf924a860f0a0995d
kenji yoshida <6b656e6a69@gmail.com>
2014-11-05 09:05:37 +0900

    Delete allgist.scala

--- allgist.scala
+++
@@ -1,4 +0,0 @@
-// scala allgist.scala > allgist.html
-val x = xml.XML.load(new java.net.URL("http://gist.github.com/api/v1/xml/gists/xuwei-k"))
-(x \\ "repo").foreach{n => println(<script src={"https://gist.github.com/"+n.text+".js"} /> )}
-
